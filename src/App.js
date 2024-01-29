cc67177ee025fec4ba15cbe4f2c0a4b9cced83cc
Perseo20220244 <a20220244@utem.edu.mx>
2024-01-29 15:03:51 -0600

    Primer commit login sin estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            isLoggedIn: false,
+            error: ''
+        };
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Aquí puedes agregar la lógica de autenticación simulada
+        const { username, password } = this.state;
+        if (username === 'usuario' && password === 'contraseña') {
+            this.setState({
+                isLoggedIn: true,
+                error: ''
+            });
+        } else {
+            this.setState({
+                isLoggedIn: false,
+                error: 'Usuario o contraseña incorrectos'
+            });
+        }
+    }
+
+    render() {
+        if (this.state.isLoggedIn) {
+            return <div>¡Has iniciado sesión correctamente!</div>;
+        }
+
+        return (
+            <div>
+                <h2>Iniciar Sesión</h2>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>Usuario:</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Contraseña:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                        />
+                    </div>
+                    <button type="submit">Iniciar Sesión</button>
+                </form>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Login;
